26265dd6e5c3b38348ec1d093658e472bd7ff2ad
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-04-04 10:25:54 -0700

    Initial Commit

---
+++ NewVagabond/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewVagabond
+//
+//  Created by Sridatt Bhamidipati on 4/4/15.
+//  Copyright (c) 2015 SRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewVagabond/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewVagabond
+//
+//  Created by Sridatt Bhamidipati on 4/4/15.
+//  Copyright (c) 2015 SRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
